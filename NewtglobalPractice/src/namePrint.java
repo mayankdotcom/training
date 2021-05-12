3e2a5c36989ea4e89d81796ab7a75176f550ee0b
mayankdotcom <57154972+mayankdotcom@users.noreply.github.com>
2021-05-12 01:42:14 -0500

    First Commit

---
+++ NewtglobalPractice/src/namePrint.java
@@ -0,0 +1,27 @@
+
+public class namePrint {
+
+	public static void main(String[] args) 
+	{
+	
+      System.out.println("hgfghfghfhfhgfghfhgf");	
+      
+     int a=100;
+                           int b=20;
+                           
+                           
+                           
+                           
+     int c;
+                                                           c=a+b;
+      
+  System.out.println(c);
+      
+      
+      
+     
+		
+	}
+
+}
+
---
+++ NewtglobalPractice/src/table.java
@@ -0,0 +1,22 @@
+public class table 
+{
+public static void main(String[] args) 
+	
+{
+	table t=new table();
+	t.sum(5,7);
+}
+
+
+public int sum(int a, int b)
+{
+int x=a+b;	
+
+return x;
+}
+
+
+
+}
+
+
